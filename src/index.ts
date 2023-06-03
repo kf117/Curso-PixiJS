fc6e3ca8d17aaa9a39b828127e6268334cb95c22
Facundo Fumis <fakufum@gmail.com>
2023-06-03 12:15:01 -0300

    Agregado resize del screen y el monoculo del pescado

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,57 @@
-import { Application,  Assets,  Sprite } from 'pixi.js'
+import { Application,  Assets,  Container,  Point,  Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
+
+
+window.addEventListener("resize", () =>
+{
+	const scaleX = window.innerWidth /app.screen.width;
+	const scaleY = window.innerHeight /app.screen.height;
+	const scale =  Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width*scale);
+	const gameHeight = Math.round(app.screen.height*scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+  
+	app.renderer.resize(gameWidth, gameHeight);				//<-------------------
+	const container = app.view.parentNode as HTMLElement;   //<-------------------
+
+	container.style.marginLeft = marginHorizontal + "px";
+	container.style.marginRight = marginHorizontal + "px";
+
+	container.style.marginTop = marginVertical + "px";
+	container.style.marginBottom = marginVertical + "px";
+});
+
+window.dispatchEvent(new Event("resize"));
 
 Assets.add("myFish", "./myFish.png");
 Assets.add("clampy", "./clampy.png");
+Assets.add("monoculo", "./monoculo.png");
 
-Assets.load(["clampy", "myFish"]).then(() =>
+
+
+Assets.load(["clampy", "myFish", "monoculo"]).then(() =>
 {
-	const clampy: Sprite = Sprite.from("myFish");
+	const fish: Sprite = Sprite.from("myFish");
+	const monoculo: Sprite = Sprite.from("monoculo");
 
-	console.log("hola mundo!", clampy.width, clampy.height);
-//clampy.anchor.set(0.5);
-		
-clampy.x = 0;
-clampy.y = 0;
+	monoculo.position = new Point(250,170);
+	const character: Container = new Container();
 
-app.stage.addChild(clampy);
+	character.scale.set(0.5);
+	character.addChild(fish);
+	character.addChild(monoculo);
+
+	app.stage.addChild(character);
 })
 
